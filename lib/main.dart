014fe1a38b53fa2df68aea56b33ac91a842e4462
pancarahmet <pancarahmet@gmail.com>
2023-12-01 22:00:36 +0300

    change primary color red

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
         scaffoldBackgroundColor: Colors.black,
         appBarTheme: AppBarTheme(backgroundColor: Colors.transparent),
       ),
